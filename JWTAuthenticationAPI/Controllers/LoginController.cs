6e1bed34fd9fc5cb1cb820d7eab6c57bc3fa11c7
Kazemi.m <Kazemi.m@PC22>
2022-01-15 16:14:26 +0330

    Add project files.

---
+++ JWTAuthenticationAPI/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using JWTAuthenticationAPI.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationAPI.Controllers
+{
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [Route("Login")]
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(UserModel model)
+        {
+            IActionResult response = Unauthorized();
+
+            if (model.Username == "admin" && model.Password == "123")
+            {
+                response = Ok(new { token = GenerateJWT(model) });
+            }
+
+            return response;
+        }
+
+        private string GenerateJWT(UserModel model)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,model.Username),
+                new Claim(JwtRegisteredClaimNames.Email,model.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+
+            };
+
+            var token = new JwtSecurityToken(
+
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ JWTAuthenticationAPI/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationAPI.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("values/Get")]
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", "value3", "value4", "value5" };
+        }
+    }
+}
